98b249e2c672c4adfb690a2e97107e76027d9b07
Billal KHETTAOUI <b.khettaoui@yahoo.com>
2015-04-24 20:38:06 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,16 @@
 ## and it contains 4 functions
 ## get, set, setinverse and getinverse
 
-
-
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
+  ## the set function changes the vector stored in the main function
   set <- function(y) {
     x <<- y
     i <<- NULL
   }
+  ## get function returns the vector x stored in the main function
   get <- function() x
+  ## Inversing the matrix using solve() 
   setinverse <- function(solve) i <<- solve
   getinverse <- function() i
   list(set = set, get = get,
@@ -26,10 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
+ ## Check if we have a matrix in the cache
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
+  ## no matrix in the cache
+  ## in this case we create an inverted matrix
   data <- x$get()
   i <- solve(data, ...)
   x$setinverse(i)
